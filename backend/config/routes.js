269163b969646963044c538148cc469e3d752a82
igor2502 <igor.gds.00@gmail.com>
2020-05-29 22:14:42 -0300

    API de artigos

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -9,13 +9,25 @@ module.exports = app => {
         
     app.route('/categories')
         .get(app.api.category.get)
-        .post(app.api.category.save)
+        .post(app.api.category.save);
 
     app.route('/categories/tree')
-        .get(app.api.category.getTree)
+        .get(app.api.category.getTree);
 
     app.route('/categories/:id')
         .get(app.api.category.getById)
         .put(app.api.category.save)
-        .delete(app.api.category.remove)
+        .delete(app.api.category.remove);
+
+    app.route('/articles')
+        .get(app.api.article.get)
+        .post(app.api.article.save);
+    
+    app.route('/articles/:id')
+        .get(app.api.article.getById)
+        .put(app.api.article.save)
+        .delete(app.api.article.remove);
+
+    app.route('/categories/:id/articles')
+        .get(app.api.article.getByCategory);
 }
